d6061650f7edcbe334c2a98e7ac86dbc0b20699a
lily <943165920@qq.com>
2019-08-22 20:17:32 +0800

    '01-项目初始化'

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入样式
+import '@/styles/index.less'
+
+// 引入element-ui
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// 让vue使用element
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
