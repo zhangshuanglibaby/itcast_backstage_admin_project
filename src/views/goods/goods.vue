39b362a9ead7cbc5d54eb706a2b9077bc4e721b1
lily <943165920@qq.com>
2019-08-31 20:25:06 +0800

    19-实现商品管理的时间格式化

--- src/views/goods/goods.vue
+++ src/views/goods/goods.vue
@@ -16,14 +16,18 @@
     >
       <el-button slot="append" icon="el-icon-search" @click="init"></el-button>
     </el-input>
-    <el-button type="success" plain>添加商品</el-button>
+    <el-button type="success" plain @click="$router.push({name:'add'})">添加商品</el-button>
     <!-- 表格 -->
     <el-table :data="goodsList" border style="width: 100%">
       <el-table-column type="index" label="序号" width="50"></el-table-column>
       <el-table-column prop="goods_name" label="商品名称" width="400"></el-table-column>
       <el-table-column prop="goods_price" label="商品价格" width="100"></el-table-column>
       <el-table-column prop="goods_weight" label="商品重量" width="100"></el-table-column>
-      <el-table-column prop="add_time" label="创建时间"></el-table-column>
+      <el-table-column prop="add_time" label="创建时间">
+        <template slot-scope="scope">
+          {{scope.row.add_time |timeFormat }}
+        </template>
+      </el-table-column>
       <el-table-column label="操作">
         <template slot-scope="scope">
           <el-button
@@ -64,7 +68,7 @@
 </template>
 <script>
 import { getAllGoods, delGood } from '@/api/goods.js'
-import { async } from 'q'
+import { timeFormat } from '@/tools/myfilters.js'
 export default {
   data () {
     return {
@@ -76,6 +80,10 @@ export default {
       goodsList: [],
       total: 0
     }
+  },
+  // 添加过滤器
+  filters: {
+    timeFormat
   },
   methods: {
     // 获取商品列表数据
@@ -110,7 +118,7 @@ export default {
     },
     // 删除商品
     delGood (row) {
-      console.log(row)
+      // console.log(row)
       this.$confirm('此操作将永久删除该文件, 是否继续?', '提示', {
         confirmButtonText: '确定',
         cancelButtonText: '取消',
